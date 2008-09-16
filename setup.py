117e0ca0b70dc7d9e3662e694d65848d6707cb2f
Yann Ramin <atrus@stackworks.net>
2008-09-15 21:19:42 -0700

    Include manifest, remove symlink, and include trove

--- setup.py
+++ setup.py
@@ -4,8 +4,21 @@ setup(
     version = "0.2.0",
     packages = find_packages(),
     author = 'Yann Ramin',
+    url = 'http://www.stackfoundry.com/other/yyafl',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: WWW/HTTP :: Dynamic Content',
+          'Topic :: Software Development :: User Interfaces',
+        ],
     author_email = 'atrus@stackworks.net',
-    description = "Yann's Yet Another Form Library is a web form validtion and metadata library",
+    description = "yyafl is a web form validtion and metadata library",
     long_description = """Yann's Yet Another Form Library is a web form validtion and metadata library.
 Forms are created as a special class of domain object, which can then be validated and data extracted.
 YYAFL believes in the separation of style from content, and as such is designed to be used
