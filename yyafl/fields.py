e2ff4ebd7f9b65083c78a90ee8af18b6700c983a
Michael Elsdoerfer <michael@elsdoerfer.com>
2010-01-12 13:41:00 +0100

    The previous commit broke the EmailField regex, actually made the whole namespace unimportable.

--- yyafl/fields.py
+++ yyafl/fields.py
@@ -113,7 +113,7 @@ class RegexField(Field):
 email_re = re.compile(
     r"(^[-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*"  # dot-atom
     r'|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-011\013\014\016-\177])*"' # quoted-string
-    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|', re.IGNORECASE)  # domain
+    r')@(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?$', re.IGNORECASE)  # domain
 
 class EmailField(RegexField):
     def __init__(self, max_length=None, min_length=None, *args, **kwargs):
